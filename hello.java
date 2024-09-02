3fbae206c91a8443e573ea622ae17588ef56c8ec
santosh <snthgmr005@gmail.com>
2024-09-02 16:58:09 +0545

    hello world in java

--- hello.java
+++ hello.java
@@ -1,6 +1,5 @@
-public class Hello {
+public class hello {
     public static void main(String[] args) {
-        System.out.println("mama ko kei");
-        
+        System.out.println("hello world");
     }
 }
